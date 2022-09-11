76510638265f7438bf1cf2f73ffa2f1c081b4788
Amen Olabode <olabodeamen4@gmail.com>
2022-09-11 15:31:50 +0100

    navbar component styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-       <h1>
-        Start
-       </h1>
+    // <Router>
+    //    <Navbar />
 
-    </div>
-   
+    //    <Routes>
+    //     <Route path='/' />
+    //    </Routes>
+
+    //   </Router>
+
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,53 @@
-import React from 'react'
+import { click } from "@testing-library/user-event/dist/click";
+import React, { useRef, useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../styles/Navbar.css";
 
 function Navbar() {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <>
-    <div className="navbar">
-        <div className="navbar-container container">
-            <Link className="navbar-logo">
-                
-            </Link>
+      <header className="header">
+        <div className="navbar-content">
+          <h2 className="navbar-logo">Anakle</h2>
+          <nav className="navbar-items">
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Our Menu</a>
+              </li>
+              <li>
+                <a href="#">Our Thoughts</a>
+              </li>
+              <li>
+                <a href="#">Our People</a>
+              </li>
+              <li>
+                <a href="#">Join Us</a>
+              </li>
+            </ul>
+
+            <button className="nav-button">Contact Us</button>
+          </nav>
+
+          <button className="nav-button toggle" onClick={handleClick}>
+            <FaBars />
+            {/* {click ? <FaTimes /> : <FaBars />} */}
+          </button>
         </div>
-    </div>
+      </header>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
