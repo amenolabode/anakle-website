a320e14b393db4f4d083e4421d7ecf7d166d06de
Amen Olabode <olabodeamen4@gmail.com>
2022-09-11 08:05:59 +0100

    added react icons and react router

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+    <div className="navbar">
+        <div className="navbar-container container">
+            <Link className="navbar-logo">
+                
+            </Link>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
