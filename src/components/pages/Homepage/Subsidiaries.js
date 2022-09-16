36f2bb6faa41661a3a26951cbc62d596f7008150
Amen Olabode <olabodeamen4@gmail.com>
2022-09-16 13:52:32 +0100

    bug fix

--- src/components/pages/Homepage/Subsidiaries.js
+++ src/components/pages/Homepage/Subsidiaries.js
@@ -9,6 +9,10 @@ import {
   forward,
 } from "./Data/subData";
 import ANAKLE from "./images/ANAKLE.svg"
+import ANAKLEFILMS from "./images/ANAKLEFILMS.svg"
+import ANAKLELABS from "./images/ANAKLELABS.svg"
+import DROVVO from "./images/DROVVO.svg"
+import FORWARD from "./images/FORWARD.svg"
 
 // import { } from "framer-;
 
@@ -42,28 +46,28 @@ const AnakleSubsidiaries = () => {
                   className="anaklefilms logo-col"
                   onClick={() => setSubsidiary(subsidiaries[1])}
                 >
-                  <img className="logos-image" src="./logo192.png" alt="" />
+                  <img className="logos-image" src={ANAKLEFILMS} alt="" />
                 </div>
                 {/*  */}
                 <div
                   className="anaklelabs logo-col"
                   onClick={() => setSubsidiary(subsidiaries[2])}
                 >
-                  <img className="logos-image" src="./anaklelabs.svg" alt="" />
+                  <img className="logos-image" src={ANAKLELABS} alt="" />
                 </div>
                 {/*  */}
                 <div
                   className="drovvo logo-col"
                   onClick={() => setSubsidiary(subsidiaries[3])}
                 >
-                  <img className="logos-image" src="./drovvo.svg " alt="" />
+                  <img className="logos-image" src={DROVVO} alt="" />
                 </div>
                 {/*  */}
                 <div
                   className="forward logo-col"
                   onClick={() => setSubsidiary(subsidiaries[4])}
                 >
-                  <img src="./forward.svg" alt="" className="logos-image" />
+                  <img src={FORWARD} alt="" className="logos-image" />
                 </div>
               </div>
             </div>
